cbc4828a74da46ac4d78cd92a5c5de8a2a790f62
aditya-chebrolu <53321066+aditya-chebrolu@users.noreply.github.com>
2020-06-06 00:24:05 -0500

    Create DominoPiling50A.java

---
+++ CodeForces/DominoPiling50A.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class DominoPiling50A {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int area = s.nextInt() * s.nextInt();
+        System.out.print(area / 2);
+    }
+}
