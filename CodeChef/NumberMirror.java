6601e4efd38caa45dcafd1c0bd14db9d2f11a62b
aditya-chebrolu <53321066+aditya-chebrolu@users.noreply.github.com>
2020-06-11 01:18:27 -0500

    Create NumberMirror.java

---
+++ CodeChef/NumberMirror.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class NumberMirror {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print(s.nextInt());
+    }
+}
