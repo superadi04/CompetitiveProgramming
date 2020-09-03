5064d331e9558f8235f015ac2dcb11bc340dfc66
Aditya Chebrolu <adityarchebrolu@gmail.com>
2020-09-03 10:14:34 -0500

    Create WeirdAlgorithm.java

---
+++ CSES Problem Set/WeirdAlgorithm.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class WeirdAlgorithm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.print(n + " ");
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            System.out.print(n + " ");
+        }
+    }
+}
