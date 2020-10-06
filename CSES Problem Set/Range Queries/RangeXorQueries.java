cd1a3a6f3e1a17b71510f4b7a2e88d90718d5df6
Aditya Chebrolu <adityarchebrolu@gmail.com>
2020-10-05 23:31:23 -0500

    Create RangeXorQueries.java

---
+++ CSES Problem Set/Range Queries/RangeXorQueries.java
@@ -0,0 +1,99 @@
+import java.io.*;
+
+public class RangeXorQueries {
+    public static void main(String[] args) throws Exception {
+        FastIO fi = new FastIO(System.in);
+        int n = fi.nextInt();
+        int q = fi.nextInt();
+        long[] arr = new long[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            arr[i] += arr[i - 1] ^ fi.nextLong();
+        }
+        for (int i = 0; i < q; i++) {
+            int a = fi.nextInt() - 1;
+            int b = fi.nextInt();
+            System.out.println(arr[b] ^ arr[a]);
+        }
+    }
+
+    static class FastIO {
+
+        InputStream dis;
+        byte[] buffer = new byte[1 << 17];
+        int pointer = 0;
+
+        public FastIO(String fileName) throws Exception {
+            dis = new FileInputStream(fileName);
+        }
+
+        public FastIO(InputStream is) throws Exception {
+            dis = is;
+        }
+
+        int nextInt() throws Exception {
+            int ret = 0;
+
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+
+            return (negative) ? -ret : ret;
+        }
+
+        long nextLong() throws Exception {
+            long ret = 0;
+
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            boolean negative = false;
+            if (b == '-') {
+                negative = true;
+                b = nextByte();
+            }
+            while (b >= '0' && b <= '9') {
+                ret = 10 * ret + b - '0';
+                b = nextByte();
+            }
+
+            return (negative) ? -ret : ret;
+        }
+
+        byte nextByte() throws Exception {
+            if (pointer == buffer.length) {
+                dis.read(buffer, 0, buffer.length);
+                pointer = 0;
+            }
+            return buffer[pointer++];
+        }
+
+        String next() throws Exception {
+            StringBuffer ret = new StringBuffer();
+
+            byte b;
+            do {
+                b = nextByte();
+            } while (b <= ' ');
+            while (b > ' ') {
+                ret.appendCodePoint(b);
+                b = nextByte();
+            }
+
+            return ret.toString();
+        }
+
+    }
+}
+
+
